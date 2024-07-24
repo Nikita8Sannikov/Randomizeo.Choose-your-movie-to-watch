ad4affb62ba87585802ac1973549d237abdf8d3a
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-07-24 10:56:47 +0600

    refactor schema to add film with addFilmOption

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -3,14 +3,14 @@ import mongoose from 'mongoose'
 const { Schema, model } = mongoose
 
 const movieSchema = new Schema({
-    id: { type: Number, required: true, unique: true }, // id фильма
+    id: { type: Number, unique: true }, // id фильма
     title: { type: String, required: true }, // Название фильма 
     img: { type: String, required: true }, // URL изображения
-    shortDescription: { type: String, required: true }, // Краткое описание фильма
-    description: { type: String, required: true }, // Полное описание фильма
-    year: { type: Number, required: true }, // Год выпуска фильма
-    genres: { type: String, required: true }, // Жанры фильма
-    rating: { type: String, required: true }, // Рейтинг фильма
+    shortDescription: String, // Краткое описание фильма
+    description: String, // Полное описание фильма
+    year: Number, // Год выпуска фильма
+    genres: String, // Жанры фильма
+    rating: String, // Рейтинг фильма
     watched: { type: Boolean, default: false } // Флаг, указывающий, просмотрен ли фильм 
   })
 
