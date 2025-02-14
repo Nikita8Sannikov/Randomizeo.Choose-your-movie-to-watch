3366d9f26e9f6dbff7b18fff4116b253488bd766
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-02-14 14:16:44 +0600

    feat: update client api

--- client/src/api.js
+++ client/src/api.js
@@ -1,19 +1,20 @@
 import {fetchApi} from "./utils/utils"
+const SERVER_API_URL = import.meta.env.VITE_SERVER_API_URL;
 
 // фильмы с главной
-export const addMovie = (movie, userId) => fetchApi(`/api/movies/add?userId=${userId}`, "POST", movie)
-export const getMovies = (userId) => fetchApi(`/api/movies?userId=${userId}`)
-export const deleteMovie = (_id, userId) => fetchApi(`/api/movies/delete/${_id}?userId=${userId}`, "DELETE")
+export const addMovie = (movie, userId) => fetchApi(`${SERVER_API_URL}/api/movies/add?userId=${userId}`, "POST", movie)
+export const getMovies = (userId) => fetchApi(`${SERVER_API_URL}/api/movies?userId=${userId}`)
+export const deleteMovie = (_id, userId) => fetchApi(`${SERVER_API_URL}/api/movies/delete/${_id}?userId=${userId}`, "DELETE")
 
 // фильмы со страницы просмотренных
-export const addWatchedMovie = (movie, userId) => fetchApi(`/api/watched-movies/add?userId=${userId}`, "POST", movie)
-export const getWatchedMovies = (userId) => fetchApi(`/api/watched-movies?userId=${userId}`)
-export const deleteWatchedMovie = (_id, userId) => fetchApi(`/api/watched-movies/delete/${_id}?userId=${userId}`, "DELETE")
+export const addWatchedMovie = (movie, userId) => fetchApi(`${SERVER_API_URL}/api/watched-movies/add?userId=${userId}`, "POST", movie)
+export const getWatchedMovies = (userId) => fetchApi(`${SERVER_API_URL}/api/watched-movies?userId=${userId}`)
+export const deleteWatchedMovie = (_id, userId) => fetchApi(`${SERVER_API_URL}/api/watched-movies/delete/${_id}?userId=${userId}`, "DELETE")
 
 // Cериалы
-export const addSeries = (movie, userId) => fetchApi(`/api/movies/series/add?userId=${userId}`, "POST", movie)
-export const getSeries = (userId) => fetchApi(`/api/movies/series?userId=${userId}`)
+export const addSeries = (movie, userId) => fetchApi(`${SERVER_API_URL}/api/movies/series/add?userId=${userId}`, "POST", movie)
+export const getSeries = (userId) => fetchApi(`${SERVER_API_URL}/api/movies/series?userId=${userId}`)
 
 // Просмотренные сериалы
-export const addWatchedSeries = (movie, userId) => fetchApi(`/api/watched-movies/series/add?userId=${userId}`, "POST", movie)
-export const getWatchedSeries = (userId) => fetchApi(`/api/watched-movies/series?userId=${userId}`)
+export const addWatchedSeries = (movie, userId) => fetchApi(`${SERVER_API_URL}/api/watched-movies/series/add?userId=${userId}`, "POST", movie)
+export const getWatchedSeries = (userId) => fetchApi(`${SERVER_API_URL}/api/watched-movies/series?userId=${userId}`)
