2fe68770b1be6e5bb09e169c43d25c374c93dd6e
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-02-17 22:01:24 +0600

    feat: add adaptive for cards and header

--- client/hooks/useArrangeCards.jsx
+++ client/hooks/useArrangeCards.jsx
@@ -5,10 +5,11 @@ export const useArrangeCards = () => {
     movieRefs.current = []
         //расположение карточек фильмов
     const  arrangeCards = (y = 0) => {
-        const cardsPerRow = window.innerWidth < 1650 ? 3 : 5
+        const viewportWidth = window.visualViewport?.width || window.innerWidth;
+        const cardsPerRow = viewportWidth > 2300 ? 7 : viewportWidth > 1540 ? 5 : viewportWidth > 1024 ? 3 : viewportWidth > 550 ? 3 : viewportWidth > 425 ? 3 : 1
         // console.log(window.innerWidth);
-        const cardWidth = window.innerWidth < 900 ? 300 : 350 // ширина карточки + расстояние между карточками
-        const cardHeight = 650 // высота карточки
+        const cardWidth = viewportWidth > 900 ? 350 : viewportWidth > 800 ? 300 :  viewportWidth > 550 ? 250 : 180 // ширина карточки + расстояние между карточками
+        const cardHeight = viewportWidth > 900 ? 550 :  viewportWidth > 550 ? 600 :  viewportWidth > 425 ? 350 : 600 // высота карточки
         let maxOffsetY = 0
     
         // console.log('Total cards:', movieRefs.current);
