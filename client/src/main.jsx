c6976b75d8773ac5dbff105bb1151301c7e238b3
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-03-27 21:54:36 +0600

    feat: add vercel insights

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import { store } from "./store"
 import App from './App.jsx'
@@ -9,6 +10,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SpeedInsights />
     <Provider store={store}>
       <App />
     </Provider>
